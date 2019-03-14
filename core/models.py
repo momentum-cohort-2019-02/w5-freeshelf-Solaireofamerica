3e9f9c71198af6bcaff308838b2f9c5dfb4000f2
Solaireofamerica <dennisdinwiddie@gmail.com>
2019-03-14 10:49:35 -0400

    working on registration

--- core/models.py
+++ core/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
 
 
 class Author(models.Model):
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render
 from .models import Book
 from django.views import generic
 
