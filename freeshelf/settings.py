597704d0e2999518ee8cc89f112b9f4ff70338fa
Solaireofamerica <dennisdinwiddie@gmail.com>
2019-03-14 13:39:44 -0400

    working on procfile issue

--- freeshelf/settings.py
+++ freeshelf/settings.py
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'freeshelf.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'freeshelf',
-        'USER': 'freeshelf',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'freeshelf',
+        # 'USER': 'freeshelf',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
