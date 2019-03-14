1bf0c9f596f4a3747b9a31ad38ac646e843e6cd6
Solaireofamerica <dennisdinwiddie@gmail.com>
2019-03-14 15:33:07 -0400

    adding heroku specific settings

--- freeshelf/settings.py
+++ freeshelf/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,5 +126,3 @@ STATIC_URL = '/static/'
 # Registration
 
 ACCOUNT_ACTIVATION_DAYS = 7
-
-django_heroku.settings(locals())
