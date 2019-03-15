5dec740fa28af9c5a3c6b4322c8597f0db67791f
Solaireofamerica <dennisdinwiddie@gmail.com>
2019-03-15 13:57:43 -0400

    getting ready to add cateories

--- core/models.py
+++ core/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
-# Create your models here.
-
 User = get_user_model()
 
 
--- freeshelf/heroku_settings.py
+++ freeshelf/heroku_settings.py
@@ -5,3 +5,13 @@ import django_heroku
 DEBUG = False
 
 django_heroku.settings(locals())
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'freeshelf',
+        'USER': 'freeshelf',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
